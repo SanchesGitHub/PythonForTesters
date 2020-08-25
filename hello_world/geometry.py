6e7375815b1cd3d59394cc60415f465954b3c6d3
as.malyshev <as.malyshev@jet.su>
2020-08-25 15:27:58 +0300

    Сортировка и вывод в консоль

--- hello_world/geometry.py
+++ hello_world/geometry.py
@@ -5,6 +5,19 @@ b = Point(3, 4)
 print(a.distance(b))
 print(a == Point(0, 0))
 print(a == b)
+print(b)
+
+
+l1 = [1, 5, 3, 7, 2, 8]
+print(l1)
+l2 = sorted(l1)
+print(l2)
+
+l3 = [b, a]
+print(l3)
+l4 = sorted(l3)
+print(l4)
+
 
 
 
