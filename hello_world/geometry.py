e78e253165592d084ef7094a7ef2ddaa733d7e6e
Alexander Malyshev <san-100@bk.ru>
2020-09-13 20:54:46 +0300

    Работа со списками и со строками

--- hello_world/geometry.py
+++ hello_world/geometry.py
@@ -26,7 +26,32 @@ print(l6)
 l7 = sorted(l3, key=lambda p: p.distance(Point(0, 0)))
 print(l7)
 
-# def cmp(a, b):
-#     return (a > b) - (a < b)
-# l8 = sorted(l3, cmp=lambda p1, p2: cmp(p1.y, p2.y))
-# print(l8)
+l8 = []
+for i in range(-5, 6):
+    l8.append(Point(i, i * i))
+
+print(l8)
+
+for el in l8:
+    print(el)
+
+l9 = []
+for el in l8:
+    l9.append(Point(el.x, -el.y))
+
+print(l9)
+
+l10 = [Point(i, i * i) for i in range(-5, 6)]
+l11 = [Point(el.x, -el.y) for el in l8]
+
+print(l10)
+print(l11)
+
+l12 = map(lambda i: Point(i, i * i), range(-5, 6))
+print(l12)
+l13 = map(lambda p: Point(p.x, -p.y), l12)
+print(l13)
+l14 = filter(lambda p: p.x >= 0, l12)
+print(l14)
+l15 = filter(lambda p: p.x % 2 == 0, l12)
+print(l15)
--- hello_world/hello.py
+++ hello_world/hello.py
@@ -5,3 +5,17 @@ def hello(s):
 hello("world")
 hello("python")
 hello("Alexander")
+
+s1 = 'test'
+s2 = '''test
+test
+test'''
+s3 = s2[1:-1]
+print(s1)
+print(s2)
+print(s3)
+print(s1.startswith("te"))
+print(s1.index("te"))
+print(s1.find("te"))
+print(s2.split("\n"))
+print(" ".join(s1))
