5e223f7928b70908bfcb876fe87fcee34165fdbe
Alexander Malyshev <san-100@bk.ru>
2020-09-27 22:14:25 +0300

    Работа со строками

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,3 +1,8 @@
+import re
+import random
+import string
+
+
 def hello(s):
     print('Hello, ' + s + '!')
 
@@ -19,3 +24,16 @@ print(s1.index("te"))
 print(s1.find("te"))
 print(s2.split("\n"))
 print(" ".join(s1))
+
+s4 = "hello\nworld"
+
+m = re.search("(.*)\n(.*)", s4)
+print(m)
+print(m.group())
+
+print(random.choice('abcd'))
+print(random.choice(string.ascii_letters + string.digits))
+
+s5 = ''.join([random.choice(string.ascii_letters + string.digits)
+              for i in range(random.randrange(20))])
+print(s5)
